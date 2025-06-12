911fd9dcdc1bf3c1b1d37a78f67174665155bac3
Dániel Buga <bugadani@gmail.com>
2025-06-12 19:41:32 +0200

    Clean up tests a bit

--- src/test.rs
+++ src/test.rs
@@ -1,76 +1,52 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use crate::{
     codegen::{self, Value},
+    error::CompileError,
     ir,
     lexer::tokenize,
     parser::parse,
     vm::{EvalContext, EvalEvent},
 };
 
-pub fn run_parser_tests(dir: impl AsRef<Path>) {
-    let dir = dir.as_ref();
-
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
+fn walk(dir: &Path, on_file: &impl Fn(&Path)) {
+    for entry in std::fs::read_dir(dir).unwrap().flatten() {
         let path = entry.path();
 
         if !filter(&path) {
             continue;
         }
 
-        if path.is_file() && path.extension().map_or(false, |ext| ext == "sm") {
-            run_compile_test(&path, false);
-        } else if path.is_dir() {
-            run_parser_tests(&path);
+        if path.is_file() {
+            on_file(&path);
+        } else {
+            walk(&path, on_file);
         }
     }
+}
+
+pub fn run_parser_tests(dir: impl AsRef<Path>) {
+    walk(dir.as_ref(), &|path| {
+        run_compile_test(path, false);
+    });
 }
 
 pub fn run_compile_tests(dir: impl AsRef<Path>) {
-    let dir = dir.as_ref();
-
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if !filter(&path) {
-            continue;
-        }
-
-        if path.is_file() && path.extension().map_or(false, |ext| ext == "sm") {
-            run_compile_test(&path, true);
-        } else if path.is_dir() {
-            run_compile_tests(&path);
-        }
-    }
+    walk(dir.as_ref(), &|path| {
+        run_compile_test(path, true);
+    });
 }
 
 pub fn run_eval_tests(dir: impl AsRef<Path>) {
-    let bless = std::env::var("BLESS").as_deref() == Ok("1");
-    let dir = dir.as_ref();
-
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if !filter(&path) {
-            continue;
+    walk(dir.as_ref(), &|path| {
+        if let Some(program) = run_compile_test(&path, true) {
+            run_eval_test(program, path);
         }
-
-        if path.is_file() && path.extension().map_or(false, |ext| ext == "sm") {
-            let program = run_compile_test(&path, true).unwrap();
-
-            run_eval_test(&program, &path, bless);
-        } else if path.is_dir() {
-            run_compile_tests(&path);
-        }
-    }
+    });
 }
 
-pub fn run_eval_test(program: &codegen::Program, path: impl AsRef<Path>, bless: bool) {
-    let stderr = path.as_ref().with_extension("stderr");
-    let fail_expected = stderr.exists();
+pub fn run_eval_test(program: codegen::Program, path: impl AsRef<Path>) {
+    let ctx = TestContext::from_path(path.as_ref());
 
     // let expressions = program
     //     .source
@@ -88,26 +64,16 @@ pub fn run_eval_test(program: &codegen::Program, path: impl AsRef<Path>, bless: 
     loop {
         match context.run() {
             EvalEvent::Error(e) => {
-                let error = e.mark(&context, "Runtime error").to_string();
-                if fail_expected && bless {
-                    std::fs::write(&stderr, error).unwrap();
-                } else if fail_expected {
-                    let expected_error = std::fs::read_to_string(&stderr).unwrap();
-                    pretty_assertions::assert_eq!(
-                        error,
-                        expected_error,
-                        "Runtime error did not match expected error."
-                    );
-                } else {
-                    panic!("{error} while running {}", path.as_ref().display());
-                }
+                let error = e.mark(&context, "Runtime error");
+
+                ctx.handle_error_string("Runtime error", error.to_string());
                 break;
             }
             EvalEvent::Complete(value) => {
                 assert!(
                     value == Value::Bool(true),
                     "Test {} exited with an unexpected value: {value:?}",
-                    path.as_ref().display()
+                    ctx.file.display()
                 );
                 break;
             }
@@ -127,100 +93,99 @@ pub fn run_eval_test(program: &codegen::Program, path: impl AsRef<Path>, bless: 
 }
 
 pub fn run_compile_test(file: impl AsRef<Path>, compile_test: bool) -> Option<codegen::Program> {
-    let bless = std::env::var("BLESS").as_deref() == Ok("1");
-    let file = file.as_ref();
-    let source = std::fs::read_to_string(file).unwrap();
+    let ctx = TestContext::from_path(file.as_ref());
 
-    let out_path = file.parent().unwrap().join(file.file_stem().unwrap());
-
-    let write_out_file = |name: &Path, content: String| {
-        _ = std::fs::create_dir_all(&name.parent().unwrap()).unwrap();
-        std::fs::write(name, strip_ansi(content)).unwrap();
-    };
-
-    let stderr = out_path.join("stderr");
-    let fail_expected = stderr.exists();
-
+    // First, parse the program.
+    let source = std::fs::read_to_string(ctx.file).unwrap();
     let tokens = tokenize(&source).collect::<Result<Vec<_>, _>>().unwrap();
-    let result = parse(&source, &tokens);
-
-    let ast = match result {
-        Ok(ast) => {
-            write_out_file(&out_path.join("ast"), format!("{:#?}", ast));
-            ast
-        }
-        Err(err) if fail_expected => {
-            compare_error(&stderr, &err, bless, file);
-            // Parsing failed as expected, no need to compile
-            return None;
-        }
-        Err(err) => {
-            if bless {
-                write_out_file(&stderr, format!("{err:?}"));
-                // Parsing failed as expected, no need to compile
-                return None;
-            } else {
-                panic!("Failed to parse {}:\n{err:?}", file.display());
-            }
-        }
+    let ast = match parse(&source, &tokens) {
+        Ok(ast) => ast,
+        Err(err) => return ctx.handle_error("Failed to parse", err),
     };
+    write_out_file(&ctx.out_path.join("ast"), format!("{:#?}", ast));
 
     if compile_test {
-        let program = ir::Program::compile(&source, &ast).unwrap();
+        // Compile the program to IR.
+        let ir = match ir::Program::compile(&source, &ast) {
+            Ok(ir) => ir,
+            Err(err) => return ctx.handle_error("Failed to compile to IR", err),
+        };
+        write_out_file(&ctx.out_path.join("ir.disasm"), ir.print());
 
-        write_out_file(&out_path.join("ir.disasm"), program.print());
+        // Compile the IR to a program.
+        let program = match codegen::compile(&source, &ir) {
+            Ok(program) => program,
+            Err(err) => return ctx.handle_error("Failed to compile", err),
+        };
+        write_out_file(
+            &ctx.out_path.join("unoptimized.disasm"),
+            program.disasm(&program.debug_info),
+        );
 
-        match codegen::compile(&source, &program) {
-            Ok(p) => {
-                write_out_file(
-                    &out_path.join("unoptimized.disasm"),
-                    p.disasm(&p.debug_info),
-                );
-                return Some(p);
-            }
-            Err(err) if fail_expected => {
-                compare_error(&stderr, &err, bless, file);
-                return None;
-            }
-            Err(err) => {
-                if bless {
-                    write_out_file(&stderr, format!("{err:?}"));
-                    return None;
-                } else {
-                    panic!("Failed to compile {}:\n{err:?}", file.display());
-                }
-            }
-        }
+        return Some(program);
     }
 
-    if fail_expected {
+    if ctx.fail_expected() {
         panic!(
             "Expected {} to fail, but it compiled successfully.",
-            file.display()
+            ctx.file.display()
         );
     }
 
     None
 }
 
-fn compare_error(stderr: &Path, err: &crate::error::CompileError, bless: bool, file: &Path) {
-    let write_out_file = |name: &Path, content: String| {
-        _ = std::fs::create_dir_all(&name.parent().unwrap()).unwrap();
-        std::fs::write(name, strip_ansi(content)).unwrap();
-    };
+struct TestContext<'a> {
+    out_path: PathBuf,
+    file: &'a Path,
+}
 
-    let error = strip_ansi(format!("{err:?}"));
-    if bless {
-        write_out_file(&stderr, error);
-    } else {
-        let expected_error = std::fs::read_to_string(&stderr).unwrap();
-        pretty_assertions::assert_eq!(
-            error,
-            expected_error,
-            "Compilation error did not match expected error. {}",
-            file.display()
-        );
+impl<'a> TestContext<'a> {
+    fn from_path(file: &'a Path) -> Self {
+        Self {
+            out_path: file.parent().unwrap().join(file.file_stem().unwrap()),
+            file,
+        }
     }
+
+    fn stderr(&self) -> PathBuf {
+        self.out_path.join("stderr")
+    }
+
+    fn fail_expected(&self) -> bool {
+        self.stderr().exists()
+    }
+
+    fn handle_error<T>(&self, failure: &str, err: CompileError<'_>) -> Option<T> {
+        self.handle_error_string(failure, format!("{err:?}"));
+        None
+    }
+
+    fn is_blessed(&self) -> bool {
+        std::env::var("BLESS").as_deref() == Ok("1")
+    }
+
+    fn handle_error_string(&self, failure: &str, error: String) {
+        let error = strip_ansi(error);
+        if self.fail_expected() || self.is_blessed() {
+            write_out_file(&self.stderr(), error);
+        } else if self.fail_expected() {
+            let expected_error = std::fs::read_to_string(&self.stderr()).unwrap();
+            pretty_assertions::assert_eq!(
+                error,
+                expected_error,
+                "Error did not match expected error. {}",
+                self.file.display()
+            );
+        } else {
+            panic!("{}: {failure}\n{error}", self.file.display());
+        }
+    }
+}
+
+fn write_out_file(name: &Path, content: String) {
+    _ = std::fs::create_dir_all(&name.parent().unwrap()).unwrap();
+    std::fs::write(name, content).unwrap();
 }
 
 pub fn strip_ansi(s: impl AsRef<str>) -> String {
@@ -240,7 +205,8 @@ pub fn strip_ansi(s: impl AsRef<str>) -> String {
 
 fn filter(path: &Path) -> bool {
     let Ok(env) = std::env::var("TEST_FILTER") else {
-        return true; // No filter set, include all paths
+        // No filter set, walk folders and somni source files.
+        return path.is_dir() || path.extension().map_or(false, |ext| ext == "sm");
     };
 
     Path::new(&env) == path
